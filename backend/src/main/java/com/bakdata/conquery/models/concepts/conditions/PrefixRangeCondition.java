fe07e678df2a355b9819e1549910ba82bc511fe4
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2021-05-19 16:25:39 +0200

    WIP towards enumerating ranges

--- backend/src/main/java/com/bakdata/conquery/models/concepts/conditions/PrefixRangeCondition.java
+++ backend/src/main/java/com/bakdata/conquery/models/concepts/conditions/PrefixRangeCondition.java
@@ -5,13 +5,18 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import javax.validation.constraints.NotEmpty;
 
 import com.bakdata.conquery.io.cps.CPSType;
 import com.bakdata.conquery.util.CalculatedValue;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.primitives.Chars;
 import io.dropwizard.validation.ValidationMethod;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
@@ -51,15 +56,41 @@ public class PrefixRangeCondition implements ConceptTreeCondition {
 		return buildStringRange(min, max);
 	}
 
+	private static final Predicate<String> ALPHANUMERIC = Pattern.compile("^\\p{Alnum}+$").asMatchPredicate();
+
 	@NotNull
 	public static Set<String> buildStringRange(@NotEmpty String min, @NotEmpty String max) {
 		final Set<String> out = new HashSet<>();
 
 		final byte[] bytes = min.getBytes(StandardCharsets.UTF_8);
+		final byte[] zero = min.getBytes(StandardCharsets.UTF_8);
 
-		final byte[] originalMin = min.getBytes(StandardCharsets.UTF_8);
+
 
 		// We are iterating every value in the range, so we're stopping as soon as possible
+		int pos = bytes.length - 1;
+
+		while(true){
+			final String value = new String(bytes);
+
+			if(value.compareTo(max) > 0){
+				break;
+			}
+
+			if (ALPHANUMERIC.test(value)) {
+				out.add(value);
+			}
+
+			bytes[pos]++;
+
+			if(bytes[pos] == Byte.MAX_VALUE){
+				bytes[pos] = Byte.MIN_VALUE;
+				pos--;
+			}
+			else {
+				pos = bytes.length - 1;;
+			}
+		}
 
 
 		return out;
